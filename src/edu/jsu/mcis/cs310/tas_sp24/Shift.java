d32ee0cf6d7561a9e85867b2fbb30118f54ec04d
Blake Sutherland <blake@robsutherland.net>
2024-02-23 16:41:42 -0600

    Initialized attributes and created constructor

--- src/edu/jsu/mcis/cs310/tas_sp24/Shift.java
+++ src/edu/jsu/mcis/cs310/tas_sp24/Shift.java
@@ -1,8 +1,30 @@
 package edu.jsu.mcis.cs310.tas_sp24;
 
-/*
+/**
  * @author blake
  */
-public class Shift {
 
+import java.time.LocalTime;
+import java.util.Map;
+
+public class Shift {
+    // initializing fields
+    private String description;
+    private int id, roundInterval, gracePeriod, dockPenalty, lunchThreshold;
+    private LocalTime shiftStart, shiftStop, lunchStart, lunchStop;
+    
+    // constructor that accepts a Map as argument
+    public Shift(Map<String, String> shiftInfo){
+        description = shiftInfo.get("description");
+        
+        shiftStart = LocalTime.parse(shiftInfo.get("shiftstart"));
+        shiftStop = LocalTime.parse(shiftInfo.get("shiftstop"));
+        lunchStart = LocalTime.parse(shiftInfo.get("luinchstart"));
+        lunchStop = LocalTime.parse(shiftInfo.get("lunchstop"));
+        
+        roundInterval = Integer.parseInt(shiftInfo.get("roundinterval"));
+        gracePeriod = Integer.parseInt(shiftInfo.get("graceperiod"));
+        dockPenalty = Integer.parseInt(shiftInfo.get("dockpenalty"));
+        lunchThreshold = Integer.parseInt(shiftInfo.get("lunchthreshold"));  
+    }
 }
