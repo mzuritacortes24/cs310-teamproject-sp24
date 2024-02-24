bce41a93e382fea54e82c8fa016a463a08fc5938
mzuritacortes24 <mzuritacortes@stu.jsu.edu>
2024-02-23 19:10:54 -0600

    Added find method for Badge

--- src/edu/jsu/mcis/cs310/tas_sp24/dao/EmployeeDAO.java
+++ src/edu/jsu/mcis/cs310/tas_sp24/dao/EmployeeDAO.java
@@ -1,13 +1,15 @@
 package edu.jsu.mcis.cs310.tas_sp24.dao;
+import edu.jsu.mcis.cs310.tas_sp24.Badge;
 import edu.jsu.mcis.cs310.tas_sp24.Employee;
+import edu.jsu.mcis.cs310.tas_sp24.EmployeeType;
+import edu.jsu.mcis.cs310.tas_sp24.Shift;
 import java.sql.*;
+import java.time.LocalDateTime;
 
 public class EmployeeDAO {
     
-    private static final String QUERY_FIND = "SELECT * FROM employee WHERE id = ?";
-
     private final DAOFactory daoFactory;
-    
+
     EmployeeDAO(DAOFactory daoFactory) {
         
         this.daoFactory = daoFactory;
@@ -15,7 +17,7 @@ public class EmployeeDAO {
     
     public Employee find(int id) {
         Employee employee = null;
-        
+        String SQL = "SELECT * FROM employee WHERE id = ?";
         PreparedStatement ps = null;
         ResultSet rs = null;
         
@@ -25,7 +27,7 @@ public class EmployeeDAO {
 
             if (conn.isValid(0)) {
 
-                ps = conn.prepareStatement(QUERY_FIND);
+                ps = conn.prepareStatement(SQL);
                 ps.setInt(1, id);
 
                 boolean hasresults = ps.execute();
@@ -37,7 +39,6 @@ public class EmployeeDAO {
                     while (rs.next()) {
                         
                     }
-
                 }
 
             }
@@ -66,6 +67,60 @@ public class EmployeeDAO {
         }
 
         
+        return employee;
+    }
+    
+    public Employee find(Badge badge)   {
+        String badgeId = badge.getId();
+        String SQL = "SELECT * FROM employee WHERE badgeid = ?";
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        
+        try { 
+
+            Connection conn = daoFactory.getConnection();
+
+            if (conn.isValid(0)) {
+
+                ps = conn.prepareStatement(SQL);
+                ps.setString(1, badgeId);
+
+                boolean hasresults = ps.execute();
+
+                if (hasresults) {
+
+                    rs = ps.getResultSet();
+
+                    while (rs.next()) {
+                        int employeeId = rs.getInt("id");
+                        return find(employeeId);  
+                    }
+
+                }
+
+            }
+
+        } catch (SQLException e) {
+
+            throw new DAOException(e.getMessage());
+
+        } finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                    throw new DAOException(e.getMessage());
+                }
+            }
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                    throw new DAOException(e.getMessage());
+                }
+            }
+
+        }
         return null;
     }
 }
