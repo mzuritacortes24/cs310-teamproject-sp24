c92d33de3b08a0bda74a3fb084bd7a8ebadadc5b
mzuritacortes24 <mzuritacortes@stu.jsu.edu>
2024-03-25 12:42:08 -0500

    Added calculateAbsenteeism!

--- src/edu/jsu/mcis/cs310/tas_sp24/dao/DAOUtility.java
+++ src/edu/jsu/mcis/cs310/tas_sp24/dao/DAOUtility.java
@@ -11,6 +11,9 @@ import edu.jsu.mcis.cs310.tas_sp24.Badge;
 import edu.jsu.mcis.cs310.tas_sp24.EventType;
 import edu.jsu.mcis.cs310.tas_sp24.Punch;
 import edu.jsu.mcis.cs310.tas_sp24.Shift;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.text.DecimalFormat;
                                          
 /**
@@ -21,7 +24,7 @@ import java.text.DecimalFormat;
  * 
  */
 public final class DAOUtility {
-
+    
     public static int calculateTotalMinutes(ArrayList<Punch> dailypunchlist, Shift shift){
         
         int totalminutes = 0;                                                   /* initialize variables to hold punches */
@@ -165,7 +168,7 @@ public final class DAOUtility {
                     totalminutes = (int) (totalminutes - shift.getLunchDuration());                                                           /* subtract lunch break from total time */
                 
                 }
-
+                
             }
         
         }
@@ -197,7 +200,7 @@ public final class DAOUtility {
     
     }
     
-    public static String getPunchListPlusTotalAsJSON(ArrayList<Punch> punchlist, Shift shift){
+    public static String getPunchListPlusTotalsAsJSON(ArrayList<Punch> punchlist, Shift shift){
         JsonObject json = new JsonObject();
         JsonArray punchArray = new JsonArray();
         
@@ -228,6 +231,120 @@ public final class DAOUtility {
         json.put("punchlist", punchArray);
         
         return Jsoner.serialize(json);
+    }
+
+    public static BigDecimal calculateAbsenteeism(ArrayList<Punch> punchList, Shift shift) {
+        //Formula: A% = (Schedule − Worked  / Schedule) × 100
+        
+        //ArrayList for each day of the week
+        ArrayList<Punch> sunday = new ArrayList<>();
+        ArrayList<Punch> monday = new ArrayList<>();
+        ArrayList<Punch> tuesday = new ArrayList<>();
+        ArrayList<Punch> wednesday= new ArrayList<>();
+        ArrayList<Punch> thursday = new ArrayList<>();
+        ArrayList<Punch> friday = new ArrayList<>();
+        ArrayList<Punch> saturday = new ArrayList<>();
+  
+        // Variables for how many minutes per day
+        double SUNDAY = 0.0, MONDAY = 0.0, TUESDAY = 0.0, WEDNESDAY = 0.0, THURSDAY = 0.0, FRIDAY = 0.0, SATURDAY = 0.0;
+        ArrayList<Double> week = new ArrayList<>();
+        LinkedHashMap<String, Double> weekday = new LinkedHashMap<>();
+        
+        // Seperates punchList into each day
+        int i = 0;
+        for (Punch punch : punchList) {
+            System.out.println(punch.printAdjusted());
+            if (null == punch.getAdjustedtimestamp().getDayOfWeek())  {
+                System.out.println("No Shift");
+            }
+            else switch (punch.getAdjustedtimestamp().getDayOfWeek())  {
+                case SUNDAY -> sunday.add(punchList.get(i));
+                case MONDAY -> monday.add(punchList.get(i));
+                case TUESDAY -> tuesday.add(punchList.get(i));
+                case WEDNESDAY -> wednesday.add(punchList.get(i));
+                case THURSDAY -> thursday.add(punchList.get(i));
+                case FRIDAY -> friday.add(punchList.get(i));
+                case SATURDAY -> saturday.add(punchList.get(i));
+                default -> System.out.println("No Shift");
+            }
+            ++i;
+        }
+        
+        // Calculates the minutes per day for each arraylist
+        i = 0;
+        while (i < 7)   {
+            switch(i)   {
+                case 0 -> {
+                    SUNDAY = calculateTotalMinutes(sunday, shift);
+                    weekday.put("SUNDAY", SUNDAY);  
+                    week.add(0, SUNDAY);
+                }
+                case 1 -> {
+                    MONDAY = calculateTotalMinutes(monday, shift);                
+                    weekday.put("MONDAY", MONDAY);
+                    week.add(1, MONDAY);
+                }
+                case 2 -> {
+                    TUESDAY = calculateTotalMinutes(tuesday, shift);
+                    weekday.put("TUESDAY", TUESDAY);        
+                    week.add(2, TUESDAY);
+                }
+                case 3 -> {
+                    WEDNESDAY = calculateTotalMinutes(wednesday, shift);                
+                    weekday.put("WEDNESDAY", WEDNESDAY);
+                    week.add(3, WEDNESDAY);
+                }
+                case 4 -> {        
+                    THURSDAY = calculateTotalMinutes(thursday, shift);
+                    weekday.put("THURSDAY", THURSDAY);
+                    week.add(4, THURSDAY);
+                }
+                case 5 -> {
+                    FRIDAY = calculateTotalMinutes(friday, shift);
+                    weekday.put("FRIDAY", FRIDAY);
+                    week.add(5, FRIDAY);
+                }
+                case 6 -> {
+                    SATURDAY = calculateTotalMinutes(saturday, shift);
+                    weekday.put("SATURDAY", SATURDAY);
+                    week.add(6, SATURDAY);
+                }   
+                default -> System.out.println("No days!!");
+            }
+            ++i;
+        }
+        
+        // Adds all the minutes of each day
+        double totalMinutes = 0.0;
+        for (double value  : week)   {
+            if (value != 0.0)   {
+                totalMinutes += value;
+            }
+            else {
+                
+            }
+        }
+        
+        System.out.println(weekday.toString());
+        double scheduledMinutes = (double) (shift.getShiftDuration() - shift.getLunchDuration());    
+
+        // totalMinutes += 300;
+        System.out.println("Total Minutes worked: "+ (totalMinutes));
+        System.out.println("Minutes Scehduled: " + (scheduledMinutes * 5));
+        System.out.println("Difference: " + ((scheduledMinutes * 5)- totalMinutes));
+        System.out.println((((scheduledMinutes * 5) - totalMinutes) / 2400 ) * 100);
+
+        BigDecimal hundred = new BigDecimal(100);
+        BigDecimal minutesWorked = new BigDecimal(totalMinutes);
+        BigDecimal minutesScheduled = new BigDecimal(scheduledMinutes * 5);
+        BigDecimal difference = minutesScheduled.subtract(minutesWorked);
+
+
+        BigDecimal absenteeismPercentage = difference
+                .divide(minutesScheduled, 2, RoundingMode.HALF_UP)
+                .multiply(hundred);
+
+        return absenteeismPercentage.setScale(4, RoundingMode.HALF_UP);
     }
      
 }
